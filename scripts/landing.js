cec3d8f8ba695dd63914c36e49a49e17ec74336f
Steven Lee-Kramer <sleekramer@gmail.com>
2016-02-15 11:25:54 -0500

    complete assigment 22: add additional transition options and refactor landing.js script

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,15 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function () {
+    "use strict";
+    var points = document.getElementsByClassName('point'), revealPoint;
+    
+    revealPoint = function (points) {
+        var i = 0;
+        for (i; i < points.length; i += 1) {
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
     };
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.mstransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.mstransform = "scaleX(1) translateY(0)";
-        points[2].style.Webkittransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    revealPoint(points);
 };
