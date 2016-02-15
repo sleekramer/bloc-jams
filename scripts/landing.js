af361a1c0b70c612c656eeb9f436652df73a26ac
Steven Lee-Kramer <sleekramer@gmail.com>
2016-02-15 12:15:39 -0500

    complete checkpoint-23: add 'scroll' event listener to window object that will run animation when triggered.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,28 @@
-var animatePoints = function () {
+
+var pointsArray = document.getElementsByClassName('point'), revealPoint;
+var animatePoints = function (points) {
     "use strict";
-    var points = document.getElementsByClassName('point'), revealPoint;
+    var i = 0;
+    for (i; i < points.length; i += 1) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+
+};
+
+window.onload = function () {
+    "use strict";
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0],
+        scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
-    revealPoint = function (points) {
-        var i = 0;
-        for (i; i < points.length; i += 1) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    window.addEventListener('scroll', function (event) {
+        if (document.documentElement.scrollTop >= scrollDistance || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
         }
-    };
-    revealPoint(points);
+    });
 };
