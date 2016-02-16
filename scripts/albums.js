6943956af41b78cb8f818e83465a7d943072f530
Steven Lee-Kramer <sleekramer@gmail.com>
2016-02-16 13:16:40 -0500

    Complete checkpoint 26: add play button event with css.

--- scripts/albums.js
+++ scripts/albums.js
@@ -30,7 +30,7 @@ var albumMarconi = {
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
-        + '  <td class="song-item-number">' + songNumber + '</td>'
+        + '  <td class="song-item-number" data-song-number="'+ songNumber +'">' + songNumber + '</td>'
         + '  <td class="song-item-title">' + songName + '</td>'
         + '  <td class="song-item-duration">' + songLength + '</td>'
         + '</tr>'
@@ -56,7 +56,21 @@ var setCurrentAlbum = function (album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
     }
 };
+// Elements we'll be adding listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+        for ( var i = 0; i <songRows.length; i += 1) {
+            songRows[i].addEventListener('mouseleave', function(event){
+                this.children[0].innerHTML = this.children[0].dataset.songNumber;
+            });
+        }
+    });
 };
